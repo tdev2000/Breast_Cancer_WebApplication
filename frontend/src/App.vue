01e603d7034a7204a24a6aa6021bbe7506b62707
tdev2000 <thaveeshadevineew@gmail.com>
2026-01-23 09:42:16 +0530

    Add gitignore and project files

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="page">
+    <!--Logo Image-->
+    <img src="@/assets/logo.png" alt="Logo" />
+    <!--Title section-->
+    <h2>
+      Clinician <span>Breast Cancer Detection System</span> For Clinicians
+    </h2>
+
+    <!--Next Button-->
+    <button @click="$emit('next')">Next</button>
+    </div>
+ </template>
+
+ <style>
+  .page {
+    background-color: #F4bcda;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  </style>
