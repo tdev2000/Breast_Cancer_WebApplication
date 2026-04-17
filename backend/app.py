dd2b2f77db279dee598d4fba597d19dde439c242
tdev2000 <thaveeshadevineew@gmail.com>
2026-04-17 10:19:10 +0530

    Add backend folder

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,14 @@
 # ----------------------------------------------
 # IMPORT REQUIRED LIBRARIES
 # ----------------------------------------------
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS
 import tensorflow as tf
 import numpy as np
 import cv2
 import os
 import sqlite3
+import re  #for validation
 
 # ----------------------------------------------
 #DATABASE CONNECTION FUCNTION
@@ -21,16 +22,30 @@ def get_db():
 # INITIALIZE FLASK APP
 # ----------------------------------------------
 app = Flask(__name__)
-CORS(
-    app,
-    resources={r"/predict": {"origins": "*"}},
-    supports_credentials=False
-)
+app.secret_key = "secret_key_123"  #required for session tracking
+CORS(app,supports_credentials=True),
 
+#Enable CORS with credentials
+CORS(app,supports_credentials=True),
+
+#----------------------------------------------
+# HOME ROUTE
+#---------------------------------------------- 
 @app.route("/", methods=["GET"])
 def home():
     return "Breast Cancer Detection API (TFLite) is running."
 
+#----------------------------------------------
+# VALIDATION FUCNTION
+#----------------------------------------------
+
+#Check email format
+def is_valid_email(email):
+    pattern = (r"[^@]+@[^@]+\.[^@]+"), email
+    return re.match(pattern, email) is not None
+
+def is_valid_password(password):
+    return len(password) >= 8
 #----------------------------------------------
 # LOGIN ROUTE
 #----------------------------------------------
@@ -42,14 +57,19 @@ def login():
 
     conn = get_db()
     doctor = conn.execute("SELECT * FROM doctors WHERE email = ? AND password = ?", 
-                          (email, password)).fetchone()
+                          (email, password)
+    ).fetchone()
     conn.close()
 
     if doctor:
+        session["doctor_id"] = doctor["id"]
+        session["doctor_name"] = doctor["name"]
+
         return jsonify({
-            "success": "Login successful", 
+            "success": "True", 
             "doctor_id": doctor["id"], 
-            "name": doctor["name"]})
+            "name": doctor["name"]
+        })
     else:
         return jsonify({"success": "False"}), 401
 
@@ -144,6 +164,9 @@ def predict():
     #GET FORM DATA FROM FRONTHEAD
     #--------------------------------------------
     doctor_id = request.form.get("doctor_id")
+
+    if not doctor_id:
+        return jsonify({"error": "Doctor ID is required"}), 401
     first_name = request.form.get("first_name")
     last_name = request.form.get("last_name")
     date_of_birth = request.form.get("date_of_birth")
