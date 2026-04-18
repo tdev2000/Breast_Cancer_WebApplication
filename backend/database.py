8810f18bf3e2444de515863a60bd6803dbb32205
tdev2000 <thaveeshadevineew@gmail.com>
2026-04-18 16:11:37 +0530

    added the smart recommendation in dtabase

--- backend/database.py
+++ backend/database.py
@@ -84,24 +84,26 @@ CREATE TABLE IF NOT EXISTS doctor_diagnosis (
 
 #Diagnosis
 cursor.execute("INSERT OR IGNORE INTO diagnosis (id, type) VALUES (1, 'Normal')")
-cursor.execute("INSERT OR IGNORE INTO diagnosis (id, type) VALUES (3, 'Benign')")
-cursor.execute("INSERT OR IGNORE INTO diagnosis (id, type) VALUES (5, 'Malignant')")
+cursor.execute("INSERT OR IGNORE INTO diagnosis (id, type) VALUES (2, 'Benign')")
+cursor.execute("INSERT OR IGNORE INTO diagnosis (id, type) VALUES (3, 'Malignant')")
 
 #---------------------------------------------
 #SMART RECCOMNDATION
 #---------------------------------------------
 
 #Malignant
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(1, 3, 5)") 
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(3, 3, 5)")
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(4, 3, 4)")             
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(1, 3, 5)") #expert radiologist
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(3, 3, 5)") #expert surgeon
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(4, 3, 4)") #SECONDARY SPECIALIST
 
 #Benign
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(2, 2, 4)") 
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(4, 2, 3)")
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(2, 2, 5)") #imaging expert 
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(4, 2, 3)") #oncologist
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(1, 2, 3)") #support doctor
 
 #Normal
-cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(2, 1, 2)")
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(2, 1, 4)")
+cursor.execute("INSERT OR IGNORE INTO doctor_diagnosis VALUES(1, 1, 3)")    
 
 #Doctors
 cursor.execute("""
@@ -112,7 +114,7 @@ INSERT OR IGNORE INTO doctors
 VALUES 
                (1, 'Dr. Dave Doe', 'dr.davedoe@example.com', 'password@123	', 'Radiologist', 'diagnostic', 1, 10, 80, 120, 'Chemotherapy, Immunotherapy', 'Ultrasound', 'Cancer Hospital', 'London', 'Mon-Fri 9am-5pm', 4.5, 'Experienced oncologist specializing in breast cancer.')
                (2, 'Dr. Jane Smith', 'dr.janesmith@example.com', 'jane098', 'Oncologist', 'medical', 1, 10, 150, 70, 'Radiation Therapy', 'MRI', 'City Hospital', 'London', 'Mon-Fri 9am-5pm', 4.8, 'Expert in imaging diagnosis.')
-               (3, 'Dr. John Doe', 'dr.johndoe@example.com', 'john543@', 'Surgen', 'surgical', 1, 20, 150, 100, 'Surgery', 'Biopsy, 'MRI', 'Royal Hospital', 'London', 'Mon-Fri 9am-5pm', 4.6, 'Cancer treatment specialist.')
+               (3, 'Dr. John Doe', 'dr.johndoe@example.com', 'john543@', 'Surgen', 'surgical', 1, 20, 150, 100, 'Surgery', 'Biopsy, MRI, Ultrasound', 'Royal Hospital', 'London', 'Mon-Fri 9am-5pm', 4.6, 'Cancer treatment specialist.')
 """)
 #---------------------------------------------
 #MATCHING 
